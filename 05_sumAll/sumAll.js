a8b0a7975ee0594ba41d0c6a2752df6b06d77e39
Hans Jio Arca <163211428+dogecheemsss@users.noreply.github.com>
2025-02-08 22:51:20 +0800

    Solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof(num1) != typeof(num2) || (num1 - Math.floor(num1)) != 0 || (num2 - Math.floor(num2)) != 0 ) {
+        return "ERROR"
+    }
+    lowerNum = Math.min(num1, num2);
+    higherNum = Math.max(num1, num2);
+    sum = 0;
+    
+    while (lowerNum <= higherNum) {
+         sum += lowerNum;
+         lowerNum++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
