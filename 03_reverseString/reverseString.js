fef390c732f69b4e2905436b2e4169cbf5b0c7ac
Hans Jio Arca <163211428+dogecheemsss@users.noreply.github.com>
2025-02-08 21:04:03 +0800

    Solve reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    stringArray = string.split('');
+    reversed = stringArray.reverse().join("").toString();
 
+    return reversed;
 };
 
 // Do not edit below this line
